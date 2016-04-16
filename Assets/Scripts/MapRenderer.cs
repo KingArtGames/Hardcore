668393f73316933baa2e883ab5ad9bad7545992c
Marius Popa <mpopa@stud.hs-bremen.de>
2016-04-16 17:52:34 +0200

    first steps for map added audio folder

--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -14,15 +14,15 @@ namespace Assets.Scripts
     public class MapRenderer : MonoBehaviour
     {
         private IMessageBus _bus;
-        private const string TILES = "";
+        private const string TILES = "Map/Tilesets/";
 
         public GameObject TilePrefab;
 
-        private Dictionary<int, Sprite> _tiles;
+        private Dictionary<string, Sprite> _tiles;
 
         public void Start()
         {
-            _tiles = new Dictionary<int, Sprite>();
+            _tiles = new Dictionary<string, Sprite>();
             _bus = Initialiser.Instance.GetService<IMessageBus>();
 
             _bus.Subscribe<LoadMapMessage>(OnMapLoaded);
@@ -30,6 +30,12 @@ namespace Assets.Scripts
 
         private void OnMapLoaded(LoadMapMessage msg)
         {
+            Sprite[] all = Resources.LoadAll<Sprite>(TILES + msg.Map.tilesets[0].name);
+            foreach (Sprite sp in all)
+            {
+                _tiles[sp.name] = sp;
+            }
+
             foreach (TileLayer layer in msg.Map.layers)
             {
                 for (int i = 0; i < layer.width; i++ )
@@ -37,16 +43,11 @@ namespace Assets.Scripts
                     for(int j = 0; j < layer.height; j++)
                     {
                         GameObject tile = Instantiate(TilePrefab);
-                        tile.transform.SetParent(transform, true);
+                        tile.transform.SetParent(gameObject.transform, true);
                         tile.transform.position = new Vector2(i * layer.width, j * layer.height);
 
-                        int id = layer.data[i*j];
-                        if (!_tiles.ContainsKey(id))
-                        {
-                            _tiles[id] = Resources.Load<Sprite>(TILES + msg.Map.tilesets[0].name + "_" + id);
-                        }
-
-                        tile.GetComponent<Image>().sprite = _tiles[id];
+                        string id = msg.Map.tilesets[0].name + "_" + layer.data[i * j];
+                        tile.GetComponent<SpriteRenderer>().sprite = _tiles[id];
                     }
                 }
             }

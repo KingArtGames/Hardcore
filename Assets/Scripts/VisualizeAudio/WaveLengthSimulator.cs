6fdb1e26ee32c375a43b18d4cef9ca09f7d5803f
V4ll3V <valle@live.de>
2016-04-16 11:43:42 +0200

    Added first Version of WaveLenghtSimulator Added StandardAssets

--- Assets/Scripts/VisualizeAudio/WaveLengthSimulator.cs
+++ Assets/Scripts/VisualizeAudio/WaveLengthSimulator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,24 +7,51 @@ public class WaveLengthSimulator : MonoBehaviour {
 
     public AudioSource AudioSource;
     public AudioListener AudioListener;
-    public int Detail = 400;
+    public int Detail = 10;
     public float Amplitude = 0.1f;
+    public float delay;
+    public Light SpotLight;
+    public int LightMultiplyer;
+
+    private float startTime;
     private Vector3 _startPostion;
+    private float _secondTimer;
+    private float _lastValue;
+    private float _velocity;
+    private float _targetValue;
+
 	void Start () {
-        _startPostion = transform.position;
+        _startPostion = transform.localScale;
+        _secondTimer = delay;
+        _lastValue = 0.0f;
 	}
 
-	void Update () {
+	void Update () 
+    {
+        if (_secondTimer <= 0)
+        {
+            SetScaleByWave();
+            _secondTimer = delay;
+        }
 
+        float newPosition = Mathf.SmoothDamp(_lastValue, _targetValue, ref _velocity, delay);
+        transform.localScale = new Vector3(newPosition, newPosition, newPosition);
+        if (SpotLight != null)
+            SpotLight.spotAngle = newPosition * LightMultiplyer;
+        _lastValue = newPosition;
+
+        _secondTimer -= Time.smoothDeltaTime;
+	}
+    
+    public void SetScaleByWave()
+    {
         float[] audioWaveInfo = new float[Detail];
         float WaveData = 0.0f;
         AudioSource.GetOutputData(audioWaveInfo, 0);
-        for(int i = 0 ; i < audioWaveInfo.Length; i++)
+        for (int i = 0; i < audioWaveInfo.Length; i++)
         {
             WaveData += System.Math.Abs(audioWaveInfo[i]);
         }
-
-        float value = _startPostion.y * WaveData * Amplitude;
-        transform.localPosition.Set(0, value, 0);
-	}
+        _targetValue = _startPostion.y + (WaveData * Amplitude);
+    }
 }

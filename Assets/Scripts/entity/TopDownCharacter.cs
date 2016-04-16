a1eb9b7840e44459c4d9287536d28a1bf362ddde
V4ll3V <valle@live.de>
2016-04-16 20:46:13 +0200

    Added stable version with sound

--- Assets/Scripts/entity/TopDownCharacter.cs
+++ Assets/Scripts/entity/TopDownCharacter.cs
@@ -6,48 +6,40 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 	[RequireComponent(typeof(Animator))]
 	public class TopDownCharacter : MonoBehaviour
 	{
-		Animator m_Animator;
-		bool m_IsGrounded;
-		float m_OrigGroundCheckDistance;
-		const float k_Half = 0.5f;
-		float m_TurnAmount;
-		float m_ForwardAmount;
-		Vector3 m_GroundNormal;
 		float m_CapsuleHeight;
 		Vector3 m_CapsuleCenter;
 		CapsuleCollider m_Capsule;
-		bool m_Crouching;
+
+        public ParticleSystem AttackRay;
+        public AudioSource AudioSource;
 
 
 		void Start()
 		{
-			m_Animator = GetComponent<Animator>();
 			m_Capsule = GetComponent<CapsuleCollider>();
 			m_CapsuleHeight = m_Capsule.height;
 			m_CapsuleCenter = m_Capsule.center;
 		}
 
-
-		public void MoveForeward(float move, bool jump)
-		{
-                transform.rotation = GetMousePosition();
-                transform.localEulerAngles = new Vector3(90, transform.localEulerAngles.y, 0);
-                transform.Translate(transform.forward * move * Time.fixedDeltaTime);
-                //ApplyExtraTurnRotation();
-		}
-        public void MoveSidewards(float move, bool jump)
+        public void FollowMouse()
         {
-           // transform.rotation = rotation;
-            //transform.localEulerAngles = new Vector3(90, transform.localEulerAngles.y, 0);
-            transform.Translate(new Vector3 (1,0,0) * move * Time.fixedDeltaTime);
+            transform.rotation = GetMousePosition();
+            transform.localEulerAngles = new Vector3(90, transform.localEulerAngles.y, 0);
         }
 
-		void ApplyExtraTurnRotation()
+		public void MoveForeward(float move)
 		{
-			// help the character turn faster (this is in addition to root rotation in the animation)
-			//float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
-			//transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+            float distance = Vector3.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            if (distance > 5.5f)
+            {
+                transform.Translate(transform.forward * move * Time.fixedDeltaTime);
+                //ApplyExtraTurnRotation();
+            }
 		}
+        public void MoveSidewards(float move)
+        {
+            transform.Translate(new Vector3 (1,0,0) * move * Time.fixedDeltaTime);
+        }
 
         private Quaternion GetMousePosition()
         {

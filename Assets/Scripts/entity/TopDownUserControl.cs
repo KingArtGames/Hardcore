cb5d0c8e3c48046c5e2241728d2f5c991c2bc624
V4ll3V <valle@live.de>
2016-04-16 16:35:56 +0200

    Added First Version of Character Controller

---
+++ Assets/Scripts/entity/TopDownUserControl.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(TopDownCharacter))]
+    public class TopDownUserControl : MonoBehaviour
+    {
+        private TopDownCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        private Vector3 m_CamForward;             // The current forward direction of the camera
+        private Vector3 m_Move;
+        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+        
+        private void Start()
+        {
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                m_Cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+            }
+
+            // get the third person character ( this should never be null due to require component )
+            m_Character = GetComponent<TopDownCharacter>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+         
+            // pass all parameters to the character control script
+            if(v != null)
+                m_Character.MoveForeward(-v*10, GetMousePosition(), m_Jump);
+            if(h != null)
+                m_Character.MoveSidewards(h*10, GetMousePosition(), m_Jump);
+
+            m_Jump = false;
+        }
+
+        private Quaternion GetMousePosition()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+
+            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector3 relativePos = targetPosition - transform.position;
+            relativePos.y = 90;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            return rotation;
+        }
+        }
+}

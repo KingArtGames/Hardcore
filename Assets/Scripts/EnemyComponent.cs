017a22dd792eeb75bfd586e25cd6a1e25f980cf6
V4ll3V <valle@live.de>
2016-04-18 16:28:36 +0200

    asd

--- Assets/Scripts/EnemyComponent.cs
+++ Assets/Scripts/EnemyComponent.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityStandardAssets.Utility;
 
 namespace Assets.Scripts
 {
@@ -13,6 +14,10 @@ namespace Assets.Scripts
         public Animator SpriteAnimator;
         public Light Spotlight;
         public SpriteRenderer HeadSprite;
+        public SphereCollider Trigger;
+        public float move = 10;
+
+        private GameObject _target;
 
         private GameEntity _gameEntity;
         public GameEntity GameEntity
@@ -25,26 +30,57 @@ namespace Assets.Scripts
         {
             HeadSprite.sprite = Resources.Load<Sprite>("Characters/enemies/enemy_" + _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value);
             //SpriteAnimator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("Animation/Enemy");
+            SetEnemyType(_gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value);
+        }
+
+        public void SetEnemyType(String musikType)
+        {
+            if (musikType == MusicTypes.metal.ToString())
+            {
+                Spotlight.color = Color.blue;
+            }
+            if (musikType == MusicTypes.classic.ToString())
+            {
+                Spotlight.color = Color.red;
+            }
+            if (musikType == MusicTypes.techno.ToString())
+            {
+                Spotlight.color = Color.green;
+            }
+        }
+        public void Update()
+        {
+            AttackPlayer();
+        }
+        public void AttackPlayer()
+        {
+            if (_target != null)
+            {
+                transform.rotation = Quaternion.LookRotation(_target.transform.position);
+                transform.localEulerAngles = new Vector3(90, transform.localEulerAngles.y, 0);
+
+                //transform.Translate(new Vector3 (0,1,0) * move * Time.fixedDeltaTime);
+            }
         }
 
         public void OnParticleCollision(GameObject other)
         {
             Debug.Log(_gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value+" | " + other.name);
-            if (CheckToKill(other))
+            if (CheckToKill(other.name))
                 Destroy(transform.parent.gameObject);
         }
 
-        public bool CheckToKill(GameObject particleSource)
+        public bool CheckToKill(String MusikType)
         {
-            if (particleSource.name.Contains(MusicTypes.metal.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.techno.ToString())
+            if (MusikType.Contains(MusicTypes.metal.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.techno.ToString())
             {
                 return true;
             }
-            else if (particleSource.name.Contains(MusicTypes.classic.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.metal.ToString())
+            else if (MusikType.Contains(MusicTypes.classic.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.metal.ToString())
             {
                 return true;
             }
-            else if (particleSource.name.Contains(MusicTypes.techno.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.classic.ToString())
+            else if (MusikType.Contains(MusicTypes.techno.ToString()) && _gameEntity.GetModule<EnemyModule>().BaseData.CurrentMusicType.Value == MusicTypes.classic.ToString())
             {
                 return true;
             }
@@ -52,5 +88,14 @@ namespace Assets.Scripts
                 return false;
             
         }
+
+        public void OnTriggerEnter(Collider collision)
+        {
+            PlayerComponent comp = collision.gameObject.GetComponentInChildren<PlayerComponent>();
+            if (comp != null)
+                _target = comp.gameObject;
+        }
+
+        
     }
 }
